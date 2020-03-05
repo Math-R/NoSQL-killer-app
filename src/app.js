f6f6d33ccd68cfd1b0e7b699d0ffd1127caaa0d0
antoineLZCH <antoinelozach.pro@gmail.com>
2020-03-05 10:16:47 +0100

    add loger and bP

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const Koa = require('koa');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = new Koa();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(ctx => {
   ctx.body = 'Hello Koa'
